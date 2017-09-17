09b7533e55f480f9bce974774672ef683dbd950a
Victor-UNLa <josevictoribanez@gmail.com>
2017-09-17 20:43:17 -0300

    Para TP
    mejoras para entrega de TP 20/9

--- Introducción a los Sistemas Operativos/Trabajos Prácticos/SO.01.PlanificadorCortoAlcance/src/Test/TestFIFO.java
+++ Introducción a los Sistemas Operativos/Trabajos Prácticos/SO.01.PlanificadorCortoAlcance/src/Test/TestFIFO.java
@@ -29,9 +29,34 @@ public class TestFIFO {
 		admP.agregarProceso("P4", 6, 1, 1, 1, Prioridad.Media);
 		admP.agregarProceso("P5", 7, 4, 5, 5, Prioridad.Alta);
 		admP.agregarProceso("P6", 8, 1, 3, 1, Prioridad.Media);
+		admP.agregarProceso("P7", 9, 1, 3, 1, Prioridad.Media);
+		admP.agregarProceso("P8", 10, 2, 1, 1, Prioridad.Alta);
 
 		//mostrarAlgoritmoFIFO() politica que ordena de acuerdo al orden de llegada
 		System.out.println(admP.mostrarAlgoritmoFIFO());
+		System.out.println(admP.mostrarAlgoritmoPrioridad());
+		
+		
+		//-Prueba numero 2 del algoritmo FIFO
+		admProcesamiento admP2 = new admProcesamiento (8, 50);
+		System.out.println("/-----------Creando 2-----------");
+
+		// agregarProceso(String proceso, int comienzaTiempo, int iCPU, int eyS, int fCPU,Prioridad prioridad)
+		admP2.agregarProceso("P1", 1, 1, 3, 2, Prioridad.Alta);
+		admP2.agregarProceso("P2", 1, 5, 2, 4, Prioridad.Baja);
+		admP2.agregarProceso("P3", 2, 4, 1, 6, Prioridad.Alta);
+		admP2.agregarProceso("P4", 3, 5, 1, 1, Prioridad.Media);
+		admP2.agregarProceso("P5", 3, 3, 4, 1, Prioridad.Media);
+		admP2.agregarProceso("P6", 5, 1, 2, 3, Prioridad.Baja);
+		admP2.agregarProceso("P6", 6, 4, 3, 1, Prioridad.Media);
+		admP2.agregarProceso("P8", 8, 2, 5, 1, Prioridad.Baja);
+		
+	
+
+		//mostrarAlgoritmoFIFO() politica que ordena de acuerdo al orden de llegada
+		System.out.println(admP2.mostrarAlgoritmoFIFO());
+		System.out.println(admP2.mostrarAlgoritmoPrioridad());
+		
 
 	}
 
